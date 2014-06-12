417eb5cbec9b38f439ac0ea1aba01d08dba0033b
Giulio <grechi@gmail.com>
2014-06-12 09:46:57 +0200

    new web files

--- web/form2.php
+++ web/form2.php
@@ -3,17 +3,19 @@
         <title>Programmazione irrigazione</title>
     </head>
     <body>
-
+      <font face='ubuntu'>
 <?php
-    $filelog = '/home/pi/pirrigami/log.txt';
+    // $filelog = '/home/pi/pirrigami/current/log.txt';
+    $filelog = '/home/giulio/Pirrigami/pirriga/log.txt';
     $file =    '/var/www/files_test/program.txt';
+    $data = date('D d M y H:i:s');
 
     $lines = array();
-    $fl = fopen($file,"r");
+    $fl = fopen($file,'r');
     $lock0 = flock($fl, LOCK_SH);
     if(!$lock0){
         echo $lock0;
-        echo "errore nel lock!";
+        echo 'errore nel lock!';
     }
     else{
         while(! feof($fl))
@@ -24,19 +26,13 @@
         flock($fl,LOCK_UN);
     }
     fclose($fl);
-    echo "<br>";
-    for ($i = 0; $i < 4; $i++){
-        echo "<br>";
-        echo "valvola ". $i. ": ". $lines[$i];
-        }
-
 
     $lastlog = array();
-    $flog = fopen($filelog,"r");
+    $flog = fopen($filelog,'r');
     $lock1 = flock($flog, LOCK_SH);
     if(!$lock1){
         echo $lock1;
-        echo "errore nel lock!<br>";
+        echo 'errore nel lock!<br>';
     }
     else{
         while(! feof($flog))
@@ -48,19 +44,68 @@
     }
     fclose($flog);
     $last = count($lastlog) - 2;
+
+    $logsplitted = array();
+    $statusswitches = array();
+    $logsplitted = explode(',', $lastlog[$last]);
+    for ($i = 0; $i < 4; $i++){
+      $statusswitches[$i] = (int) $logsplitted[$i + 1];
+    }
+    for ($i = 0; $i < 4; $i++){
+      $statusswitches[$i + 4] = (int) $logsplitted[$i + 9];
+    }
+
+    $linessplitted = array();
+    for ($i = 0; $i < 4; $i++){
+       $linessplitted[$i] = explode(',', $lines[$i]);
+    }
+
+    echo '<h2>Impostazione attuale valvole:</h2><table>';
+    for ($i = 0; $i < 4; $i++ ){
+      echo '<tr><td><br></td></tr>';
+      echo '<tr><td><b><u>'. 'valvola '. $i. ': '. '</u></b></td>';
+      if ($statusswitches[$i] == 1){
+        echo '<td>interruttore su manuale</td>';
+      }
+      if ($statusswitches[$i] == 0){
+        echo '<td>interruttore su automatico</td>';
+      }
+      echo '</tr>';
+      echo '<tr><td><b>stato attuale:</b></td>';
+      if ($statusswitches[$i + 4] == 0){
+        echo '<td>spenta</td>';
+      }
+      if ($statusswitches[$i + 4] == 1){
+        echo '<td><b>ACCESA</b></td>';
+      }
+      echo '</tr>';
+      echo '<tr>'. '<td><b>impostata su:</b></td><td>'. $linessplitted[$i][1]. '</td></tr>';
+      echo '<tr>'.'<td><b>giorni:</b></td>';
+      echo '<td>';
+      for ($k = 6; $k < (count($linessplitted[$i])); $k++){
+          echo $linessplitted[$i][$k]. ' ';
+      }
+      echo '</td>';
+      echo '</tr>';
+      echo '<tr><td><b>alle ore: </b></td>';
+      echo '<td>'. $linessplitted[$i][2]. ':'. $linessplitted[$i][3]. '</td></tr>';
+      echo '<tr><td><b>per:</b></td><td>'. $linessplitted[$i][4]. ':'. $linessplitted[$i][5]. '</td></tr>';
+    }
+    echo '</table>';
 ?>
 
+
 		<h2>Regolazione valvole</h2>
-		<form action="process.php" method="post">
+		<form action='process.php' method='post'>
 			<br>Valvola da impostare:
-	       <select name="numerovalvola">
-	       <option value="0">0</option>
-	       <option value="1">1</option>
-	       <option value="2">2</option>
-	       <option value="3">3</option>
+	       <select name='numerovalvola'>
+	       <option value='0'>0</option>
+	       <option value='1'>1</option>
+	       <option value='2'>2</option>
+	       <option value='3'>3</option>
 	       <br>
-			<input type="radio" name="onoff" value="on">on
-			<input type="radio" name="onoff" value="off" checked="yes">off<p>
+			<input type='radio' name='onoff' value='on'>on
+			<input type='radio' name='onoff' value='off' checked='yes'>off<p>
 
 		<table>
 			<tr>
@@ -68,81 +113,82 @@
 				</td>
 				<td>
 				ora
-				<select name="ora">
-	       <option value="00">00</option>
-	       <option value="01">01</option>
-	       <option value="02">02</option>
-	       <option value="03">03</option>
-	       <option value="04">04</option>
-	       <option value="05">05</option>
-	       <option value="06">06</option>
-	       <option value="07">07</option>
-	       <option value="08">08</option>
-	       <option value="09">09</option>
-	       <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-          <option value="13">13</option>
-          <option value="14">14</option>
-          <option value="15">15</option>
-          <option value="16">16</option>
-          <option value="17">17</option>
-          <option value="18">18</option>
-          <option value="19">19</option>
-          <option value="20">20</option>
-          <option value="21">21</option>
-          <option value="22">22</option>
-          <option value="23">23</option>
+				<select name='ora'>
+	       <option value='00'>00</option>
+	       <option value='01'>01</option>
+	       <option value='02'>02</option>
+	       <option value='03'>03</option>
+	       <option value='04'>04</option>
+	       <option value='05'>05</option>
+	       <option value='06'>06</option>
+	       <option value='07'>07</option>
+	       <option value='08'>08</option>
+	       <option value='09'>09</option>
+	       <option value='10'>10</option>
+          <option value='11'>11</option>
+          <option value='12'>12</option>
+          <option value='13'>13</option>
+          <option value='14'>14</option>
+          <option value='15'>15</option>
+          <option value='16'>16</option>
+          <option value='17'>17</option>
+          <option value='18'>18</option>
+          <option value='19'>19</option>
+          <option value='20'>20</option>
+          <option value='21'>21</option>
+          <option value='22'>22</option>
+          <option value='23'>23</option>
           </select>
 				Minuti:
-	       <select name="minuti">
-	       <option value="00">00</option>
-	       <option value="15">15</option>
-	       <option value="30">30</option>
-	       <option value="45">45</option>
+	       <select name='minuti'>
+	       <option value='00'>00</option>
+	       <option value='15'>15</option>
+	       <option value='30'>30</option>
+	       <option value='45'>45</option>
           </select>
           </td>
 			</tr>
 			<tr>
 				<td>Durata: </td>
 				<td>ore
-          <select name="durataore">
-            <option value="00">00</option>
-	       <option value="01">01</option>
-	       <option value="02">02</option>
-	       <option value="03">03</option>
-	       <option value="04">04</option>
-	       <option value="05">05</option>
-	       <option value="06">06</option>
-	       <option value="07">07</option>
-	       <option value="08">08</option>
+          <select name='durataore'>
+            <option value='00'>00</option>
+	       <option value='01'>01</option>
+	       <option value='02'>02</option>
+	       <option value='03'>03</option>
+	       <option value='04'>04</option>
+	       <option value='05'>05</option>
+	       <option value='06'>06</option>
+	       <option value='07'>07</option>
+	       <option value='08'>08</option>
 
           </select>
 			 Minuti:
-	       <select name="durataminuti">
-	       <option value="00">00</option>
-	       <option value="15">15</option>
-	       <option value="30">30</option>
-	       <option value="45">45</option>
+	       <select name='durataminuti'>
+	       <option value='00'>00</option>
+	       <option value='15'>15</option>
+	       <option value='30'>30</option>
+	       <option value='45'>45</option>
           </select>
           </td>
 		</tr>
 	</table>
 			<p>
-          <input type="checkbox" name="lun" value="si">lun
-          <input type="checkbox" name="mar" value="si">mar
-          <input type="checkbox" name="mer" value="si">mer
-          <input type="checkbox" name="gio" value="si">gio
-          <input type="checkbox" name="ven" value="si">ven
-          <input type="checkbox" name="sab" value="si">sab
-          <input type="checkbox" name="dom" value="si">dom<p>
-		<input type="submit"/>
+          <input type='checkbox' name='lun' value='si'>lun
+          <input type='checkbox' name='mar' value='si'>mar
+          <input type='checkbox' name='mer' value='si'>mer
+          <input type='checkbox' name='gio' value='si'>gio
+          <input type='checkbox' name='ven' value='si'>ven
+          <input type='checkbox' name='sab' value='si'>sab
+          <input type='checkbox' name='dom' value='si'>dom<p>
+		<input type='submit'/>
 		</form>
         <br>
         <?php
-            echo $lastlog[$last];
-            echo $lastlog[$last + 1];
-            echo '<br>'.$data;
+            // echo $lastlog[$last];
+            // echo '<br>'.$data;
+            echo '<br><br>ultimo log scritto: '. $logsplitted[0];
         ?>
+        </font>
 	</body>
 </html>
--- web/process.php
+++ web/process.php
@@ -1,42 +1,46 @@
 
 <html>
 <body>
-
-<?
-    date_default_timezone_set("Europe/Rome");
+<font face="ubuntu">
+<?php
+    date_default_timezone_set('Europe/Rome');
 
     $file =          '/var/www/files_test/program.txt';
-    $data =          date("D d M y H:i:s");
+    // $filelog =       '/home/pi/pirrigami/current/log.txt';
+    $filelog =       '/home/giulio/Pirrigami/pirriga/log.txt';
+    $data =          date('D d M y H:i:s');
     $numerovalvola = $_POST['numerovalvola'];
     $onoff =         $_POST['onoff'];
     $ora    =        $_POST['ora'];
     $minuti =        $_POST['minuti'];
     $durataore    =  $_POST['durataore'];
     $durataminuti =  $_POST['durataminuti'];
-    $days = "";
-    if ($_POST['lun'] == "si"){$days .= "lunedi,";}
-    if ($_POST['mar'] == "si"){$days .= "martedi,";}
-    if ($_POST['mer'] == "si"){$days .= "mercoledi,";}
-    if ($_POST['gio'] == "si"){$days .= "giovedi,";}
-    if ($_POST['ven'] == "si"){$days .= "venerdi,";}
-    if ($_POST['sab'] == "si"){$days .= "sabato,";}
-    if ($_POST['dom'] == "si"){$days .= "domenica";}
+    $days = '';
+    if ($_POST['lun'] == 'si'){$days .= 'lunedi,';}
+    if ($_POST['mar'] == 'si'){$days .= 'martedi,';}
+    if ($_POST['mer'] == 'si'){$days .= 'mercoledi,';}
+    if ($_POST['gio'] == 'si'){$days .= 'giovedi,';}
+    if ($_POST['ven'] == 'si'){$days .= 'venerdi,';}
+    if ($_POST['sab'] == 'si'){$days .= 'sabato,';}
+    if ($_POST['dom'] == 'si'){$days .= 'domenica';}
 
-    echo 'Ora:       '.               $data ."<br>";
-    echo "<br>";
-    echo "numero valvola impostata:". $numerovalvola. "<br>";
-    echo "accensione: ".              $onoff. "<br>";
-    echo "ora impostata: ".           $ora. ":". $minuti. "<br>";
-    echo "durata impostata: ".        $durataore. " ore ". $durataminuti. " minuti <br>";
-    echo "giorni impostati: ".        $days. "<br>";
-    $line = $data. ",". $onoff. ",". $ora. ",". $minuti. ",". $durataore. ",". $durataminuti. ",". $days;
+    echo 'Ora: '. $data .'<br><br>';
+    echo '<table>';
+    echo '<tr><td>'. 'valvola impostata:'.'</td><td>'. $numerovalvola. '<br>'. '</td></tr>';
+    echo '<tr><td>'. 'impostata su: '.    '</td><td>'. $onoff. '<br>'. '</td></tr>';
+    echo '<tr><td>'. 'ora impostata: '.   '</td><td>'. $ora. ':'. $minuti. '<br>'. '</td></tr>';
+    echo '<tr><td>'. 'durata impostata: '.'</td><td>'. $durataore. ' ore '. $durataminuti. ' minuti <br>'. '</td></tr>';
+    echo '<tr><td>'. 'giorni impostati: '. '</td><td>'.$days. '<br>'. '</td></tr>';
+    echo '</table>';
+    $line = $data. ','. $onoff. ','. $ora. ','. $minuti. ','. $durataore. ','. $durataminuti. ','. $days;
 
     $lines = array();
-    $fl = fopen($file,"r");
+    $fl = fopen($file,'r');
     $lock0 = flock($fl, LOCK_SH);
     if(!$lock0){
         echo $lock0;
-        echo "errore nel lock!";
+        echo 'errore nel lock!';
+        echo '<br>programmazione non salvata';
     }
     else{
         while(! feof($fl))
@@ -47,16 +51,16 @@
         flock($fl,LOCK_UN);
     }
     fclose($fl);
-    echo "<br>";
-    for ($i = 0; $i < 4; $i++){
-        if ($numerovalvola == $i){
-            echo "<br>----> ";
-        }
-        else{
-            echo "<br>";
-        }
-        echo "valvola ". $i. ": ". $lines[$i];
-        }
+    echo '<br>';
+    // for ($i = 0; $i < 4; $i++){
+    //     if ($numerovalvola == $i){
+    //         echo '<br>----> ';
+    //     }
+    //     else{
+    //         echo '<br>';
+    //     }
+    //     echo 'valvola '. $i. ': '. $lines[$i];
+    //     }
 
     $lines[$numerovalvola] = $line;
 
@@ -64,37 +68,38 @@
     $lock = flock($program, LOCK_EX);
     if(!$lock){
         echo $lock;
-        echo "errore nel lock!";
+        echo 'errore nel lock!';
     }
     else{
         for ($i = 0; $i < 4; $i++){
             fwrite($program, $lines[$i]. PHP_EOL);
         }
-        echo "<br><br>Scritto ($line) nel file";
+        echo '<h3>nuova programmazione salvata</h3>';
+        // echo '<br><br>Scritto ('. $line. ') nel file';
         flock($program,LOCK_UN);
 
     }
     fclose($program);
-    echo "<br><br>";
-    echo "nuova impostazione:";
-    for ($i = 0; $i < 4; $i++){
-        if ($numerovalvola == $i){
-            echo "<br>----> ";
-        }
-        else{
-            echo "<br>";
-        }
-        echo "valvola ". $i. ": ". $lines[$i];
-        }
+
+    // echo '<h4>nuova impostazione:</h4>';
+    // for ($i = 0; $i < 4; $i++){
+    //     if ($numerovalvola == $i){
+    //         echo '----> ';
+    //     }
+    //     else{
+    //         echo '';
+    //     }
+    //     echo 'valvola '. $i. ': '. $lines[$i]. '<br>';
+    //     }
 
     sleep(1); #per prendere l'ultima riga aggiornata
-    $filelog = "/home/pi/pirrigami/log.txt";
+
     $lastlog = array();
-    $flog = fopen($filelog,"r");
+    $flog = fopen($filelog,'r');
     $lock1 = flock($flog, LOCK_SH);
     if(!$lock1){
         echo $lock1;
-        echo "errore nel lock!";
+        echo 'errore nel lock!';
     }
     else{
         while(! feof($flog))
@@ -105,13 +110,62 @@
         flock($flog,LOCK_UN);
     }
     fclose($flog);
-    echo "<br>";
-    echo "<br>";
     $last = count($lastlog) - 2;
-    echo $lastlog[$last];
-    echo $lastlog[$last + 1];
-    echo '<br>'.$data;
+    echo '<br>';
+
+    $logsplitted = array();
+    $statusswitches = array();
+    $logsplitted = explode(',', $lastlog[$last]);
+    for ($i = 0; $i < 4; $i++){
+      $statusswitches[$i] = (int) $logsplitted[$i + 1];
+    }
+    for ($i = 0; $i < 4; $i++){
+      $statusswitches[$i + 4] = (int) $logsplitted[$i + 9];
+    }
+
+    $linessplitted = array();
+    for ($i = 0; $i < 4; $i++){
+       $linessplitted[$i] = explode(',', $lines[$i]);
+    }
+
+    echo '<h2>Impostazione attuale valvole:</h2><table>';
+    for ($i = 0; $i < 4; $i++ ){
+      echo '<tr><td><br></td></tr>';
+      echo '<tr><td><b><u>'. 'valvola '. $i. ': '. '</u></b></td>';
+      if ($statusswitches[$i] == 1){
+        echo '<td>interruttore su manuale</td>';
+      }
+      if ($statusswitches[$i] == 0){
+        echo '<td>interruttore su automatico</td>';
+      }
+      echo '</tr>';
+      echo '<tr><td><b>stato attuale:</b></td>';
+      if ($statusswitches[$i + 4] == 0){
+        echo '<td>spenta</td>';
+      }
+      if ($statusswitches[$i + 4] == 1){
+        echo '<td><b>ACCESA</b></td>';
+      }
+      echo '</tr>';
+      echo '<tr>'. '<td><b>impostata su:</b></td><td>'. $linessplitted[$i][1]. '</td></tr>';
+      echo '<tr>'.'<td><b>giorni:</b></td>';
+      echo '<td>';
+      for ($k = 6; $k < (count($linessplitted[$i])); $k++){
+          echo $linessplitted[$i][$k]. ' ';
+      }
+      echo '</td>';
+      echo '</tr>';
+      echo '<tr><td><b>alle ore: </b></td>';
+      echo '<td>'. $linessplitted[$i][2]. ':'. $linessplitted[$i][3]. '</td></tr>';
+      echo '<tr><td><b>per:</b></td><td>'. $linessplitted[$i][4]. ':'. $linessplitted[$i][5]. '</td></tr>';
+    }
+    echo '</table><br>';
+    echo 'ultimo log scritto: '. $logsplitted[0];
+
+
+
 ?><br>
+</font>
 </body>
 </html>
 
